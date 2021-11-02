5a6a1fdf239658b91fbc1f26337783203205a891
Kunal Shah <meetups4kunal@gmail.com>
2021-11-01 21:58:08 -0400

    All SimpleBank tests are passing. Checking in to Git Classroom
    kunalrshah
    
    All SimpleBank tests are passing. Checking in to Git Classroom

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -4,7 +4,8 @@
  * https://solidity.readthedocs.io/en/latest/080-breaking-changes.html
  */
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.5.16 <0.9.0;
+// pragma solidity >=0.5.16 <0.9.0;
+pragma solidity >=0.7.0 <0.9.0;
 
 contract SimpleBank {
 
@@ -14,48 +15,54 @@ contract SimpleBank {
     
     // Fill in the visibility keyword. 
     // Hint: We want to protect our users balance from other contracts
-    mapping (address => uint) balances ;
+		// KS "private" added
+    mapping (address => uint) private balances ;
     
     // Fill in the visibility keyword
     // Hint: We want to create a getter function and allow contracts to be able
     //       to see if a user is enrolled.
-    mapping (address => bool) enrolled;
+		// KS "public" added
+    mapping (address => bool) public enrolled;
 
     // Let's make sure everyone knows who owns the bank, yes, fill in the
     // appropriate visilibility keyword
-    address owner = msg.sender;
+		// KS "public" added
+    address public owner = msg.sender;
     
     /* Events - publicize actions to external listeners
      */
     
     // Add an argument for this event, an accountAddress
-    event LogEnrolled();
+    event LogEnrolled(address accountAddress); // review the naming convention, preceding underscore or not
 
     // Add 2 arguments for this event, an accountAddress and an amount
-    event LogDepositMade();
+    event LogDepositMade(address accountAddress, uint256 amount);
 
     // Create an event called LogWithdrawal
     // Hint: it should take 3 arguments: an accountAddress, withdrawAmount and a newBalance 
-    event LogWithdrawal();
+    event LogWithdrawal(address accountAddress, uint256 withdrawAmount, uint256 newBalance);
 
     /* Functions
      */
+
+		receive () external payable { } 
 
     // Fallback function - Called if other functions don't match call or
     // sent ether without data
     // Typically, called when invalid data is sent
     // Added so ether sent to this contract is reverted if the contract fails
     // otherwise, the sender's money is transferred to contract
-    function () external payable {
+    fallback () external payable {
         revert();
     }
 
     /// @notice Get balance
     /// @return The balance of the user
-    function getBalance() public returns (uint) {
+    function getBalance() public view returns (uint) {
       // 1. A SPECIAL KEYWORD prevents function from editing state variables;
       //    allows function to run locally/off blockchain
       // 2. Get the balance of the sender of this transaction
+			return(balances[msg.sender]);
     }
 
     /// @notice Enroll a customer with the bank
@@ -63,11 +70,19 @@ contract SimpleBank {
     // Emit the appropriate event
     function enroll() public returns (bool){
       // 1. enroll of the sender of this transaction
+			address senderAddr = msg.sender;
+			bool enrolledTorF = true;
+			enrolled[senderAddr] = enrolledTorF;
+			emit LogEnrolled(senderAddr);
+			return (enrolledTorF);
     }
 
     /// @notice Deposit ether into bank
-    /// @return The balance of the user after the deposit is made
-    function deposit() public returns (uint) {
+    /// @return The balance of the user after the deposit is made 
+		function deposit() 
+							public 
+							payable 
+							returns (uint) {
       // 1. Add the appropriate keyword so that this function can receive ether
     
       // 2. Users should be enrolled before they can make deposits
@@ -78,13 +93,29 @@ contract SimpleBank {
       // 4. Emit the appropriate event associated with this function
 
       // 5. return the balance of sndr of this transaction
-    }
+			address senderAddr = msg.sender;
+
+//			require((!buyOrdersFulfilled[buyOrderId]), "Error: Buy Order already fulfilled.");
+			require(enrolled[senderAddr], "Error: Msg Sender is not an enrolled customer of the Bank.");
+			uint acctBalance = balances[senderAddr];
+
+			uint depositAmount = msg.value;
+
+			balances[senderAddr] += depositAmount;
+			acctBalance += depositAmount;
+
+			emit LogDepositMade(senderAddr, depositAmount); 
+			return(acctBalance);
+    } // deposit()
 
     /// @notice Withdraw ether from bank
     /// @dev This does not return any excess ether sent to it
     /// @param withdrawAmount amount you want to withdraw
     /// @return The balance remaining for the user
-    function withdraw(uint withdrawAmount) public returns (uint) {
+    function withdraw(uint withdrawAmount) 
+								public 
+								payable
+								returns (uint) {
       // If the sender's balance is at least the amount they want to withdraw,
       // Subtract the amount from the sender's balance, and try to send that amount of ether
       // to the user attempting to withdraw. 
@@ -96,5 +127,18 @@ contract SimpleBank {
       //    sender's balance
 
       // 3. Emit the appropriate event for this message
-    }
-}
+			address senderAddr = msg.sender;
+			uint currentBalance = balances[senderAddr];
+
+			require(currentBalance >= withdrawAmount, "Bank account doesn't have enough money to fulfill this withdrawal");
+
+
+			(bool sent, bytes memory callData) = senderAddr.call{value: msg.value}("");
+			require(sent, "Failed to transfer Ether to the Msg Sedner");
+
+			balances[senderAddr] -= withdrawAmount;
+			uint remainingBalance = currentBalance - withdrawAmount;
+			emit LogWithdrawal(senderAddr, withdrawAmount, remainingBalance);
+			return(remainingBalance);
+    } // withdraw()
+} // SimpleBank
--- truffle-config.js
+++ truffle-config.js
@@ -5,11 +5,24 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     }
-  }
-  // // To run contract with the latest compiler, uncomment lines 10-14 below:
-  // compilers: { 
-  //   solc: {
-  //     version: "^0.8",    // Fetch latest 0.8.x Solidity compiler 
-  //   }
-  // }
+  },
+  compilers: {
+    solc: {
+       version: "0.8.0",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+       settings: {          // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+         enabled: false,
+          runs: 200
+        },
+        evmVersion: "byzantium"
+       }
+    }
+  },
+// // To run contract with the latest compiler, uncomment lines 10-14 below:
+// compilers: { 
+//   solc: {
+//     version: "^0.8",    // Fetch latest 0.8.x Solidity compiler 
+//   }
+// }
 };
